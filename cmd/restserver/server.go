7fd039ce50e376c57bde1ff9ad6b91a20b1dfb68
La Prise <gaurondorian@gmail.com>
2022-04-06 15:14:12 +0200

    :test_tube: Test -> Ajout des entities dans server.go

--- cmd/restserver/server.go
+++ cmd/restserver/server.go
@@ -4,15 +4,18 @@ import (
 	"fmt"
 	"net/http"
 
+	"internal/entities"
+
 	"github.com/gorilla/mux"
 )
 
 func main() {
 
-	fmt.Println("🚀 Lancement de l'api ...")
+	fmt.Println("🚀 Lancement de l'api sur le port 8080...")
 
 	r := mux.NewRouter()
 	r.HandleFunc("/hello", helloHandler)
+	r.HandleFunc("/language", testLanguage)
 
 	http.ListenAndServe(":8080", r)
 
@@ -20,5 +23,15 @@ func main() {
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
+
 	fmt.Fprintf(w, "Bonjour depuis l'api")
+}
+
+func testLanguage(w http.ResponseWriter, r *http.Request) {
+
+	var language entities.Language = entities.NewLanguage("21", "Golang")
+
+	fmt.Printf("Coucou from testLanguage : %s", language.String())
+
+	fmt.Fprintf(w, "Test : %s", language.String())
 }
