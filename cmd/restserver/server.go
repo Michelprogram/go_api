587d0d4d087082a3fe61abead6301ba4e89ac6de
La Prise <gaurondorian@gmail.com>
2022-04-06 14:34:08 +0200

    :test_tube: Test -> gorilla route /hello

--- cmd/restserver/server.go
+++ cmd/restserver/server.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("test")
+
+	fmt.Println("🚀 Lancement de l'api ...")
+
+	r := mux.NewRouter()
+	r.HandleFunc("/hello", helloHandler)
+
+	http.ListenAndServe(":8080", r)
+
+	fmt.Println("Fermeture de l'api ...")
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Bonjour depuis l'api")
 }
--- go.mod
+++ go.mod
@@ -2,13 +2,16 @@ module go_api
 
 go 1.15
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require github.com/gorilla/mux v1.8.0
 
 require internal/entities v1.0.0
+
 replace internal/entities => ./internal/entities
 
 require internal/persistence v1.0.0
+
 replace internal/persistence => ./internal/persistence
 
 require internal/web/rest v1.0.0
+
 replace internal/web/rest => ./internal/web/rest
